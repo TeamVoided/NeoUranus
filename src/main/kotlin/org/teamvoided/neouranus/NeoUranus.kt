85d6fe37a8248016e442b32733c85fa04bd76527
TheEnderCore <theendercore@gmail.com>
2025-06-17 15:02:41 +0300

    fixed boot crash

--- src/main/kotlin/org/teamvoided/neouranus/NeoUranus.kt
+++ src/main/kotlin/org/teamvoided/neouranus/NeoUranus.kt
@@ -1,15 +1,13 @@
 package org.teamvoided.neouranus
 
-import org.teamvoided.neouranus.client.NeoUClient
 import net.minecraft.resources.ResourceLocation
+import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import org.teamvoided.neouranus.init.NeoUBlocks
-import org.teamvoided.neouranus.init.NeoUCreativeTabs
-import org.teamvoided.neouranus.init.NeoUEntities
-import org.teamvoided.neouranus.init.NeoUItems
-import org.teamvoided.neouranus.init.NeoUMisc
+import org.teamvoided.neouranus.client.NeoUClient
+import org.teamvoided.neouranus.init.*
+import thedarkcolour.kotlinforforge.forge.runWhenOn
 
 @Mod(NeoUranus.ID)
 object NeoUranus {
@@ -17,7 +15,7 @@ object NeoUranus {
 
     // the logger for our mod
     val log: Logger = LogManager.getLogger(ID)
-    fun id(path: String) = ResourceLocation(ID, path)
+    fun id(path: String) = ResourceLocation.fromNamespaceAndPath(ID, path)
 
     init {
         log.info("Loading NeoUranus")
@@ -28,7 +26,6 @@ object NeoUranus {
         NeoUCreativeTabs
         NeoUMisc
 
-
-        NeoUClient
+        runWhenOn(Dist.CLIENT) { NeoUClient }
     }
 }
--- src/main/kotlin/org/teamvoided/neouranus/util/NeodymiumHelper.kt
+++ src/main/kotlin/org/teamvoided/neouranus/util/NeodymiumHelper.kt
@@ -4,10 +4,10 @@ import net.minecraft.world.item.ItemStack
 
 fun setPolarity(stack: ItemStack, scarlet: Boolean) {
     val tag = stack.getOrCreateTag()
-    tag.putBoolean("Polarity", scarlet)
+    tag.putBoolean("polarity", scarlet)
 }
 
 fun isScarlet(stack: ItemStack): Boolean {
     val tag = stack.tag
-    return tag?.getBoolean("Polarity") ?: false
+    return tag?.getBoolean("polarity") ?: false
 }
